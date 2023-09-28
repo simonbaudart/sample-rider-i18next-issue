c0b06be621641fb1e0dfb80fd39c60a6ff9af4b0
Simon Baudart <s.baudart@wavenet.be>
2023-09-28 11:30:56 +0200

    Sample project

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,13 @@
+﻿/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+    defaultLocale: "fr",
+    locales: ["fr"],
+    load: ["server", "client"],
+    i18nextServerPlugins: {
+        "{initReactI18next}": "react-i18next",
+    },
+    i18nextClientPlugins: {
+        "{initReactI18next}": "react-i18next",
+    },
+    namespaces: ["translation"],
+};
